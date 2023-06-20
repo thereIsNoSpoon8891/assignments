2ace38d2bf9bd6f6c695b04689d9c00abfd66a76
Shane Rowland <shanerowland8891@gmail.com>
2023-06-19 17:40:03 -0700

    entire level 1 commit

---
+++ moms-shopping-list/index.js
@@ -0,0 +1,60 @@
+let list = document.addItem
+
+list.addEventListener("submit", function(e){
+    e.preventDefault() //prevents refresh
+
+    let addedItem = list.title.value  //grabs text from input box
+    list.title.value = ""           // clears form after entered
+    // 3 step process 
+    // 1. createElement, capture in a variable 
+    // 2. edit the content: variable.textcontent 
+    // 3. append it: document.querySelector('ID').append(variable)
+
+    let newItem = document.createElement('li') // step 1
+    
+    newItem.textContent = addedItem             // step 2
+    
+    document.getElementById('list').append(newItem) // step 3 
+   
+    let newEditBox = document.createElement('button')// step 1
+
+    newEditBox.textContent = 'edit'                 // step 2
+
+    document.getElementById('list').append(newEditBox) // step 3
+
+    let newDeleteButton = document.createElement('button') // step 1
+
+    newDeleteButton.textContent = 'X'                       // step 2
+
+    document.getElementById('list').append(newDeleteButton) // step 3
+    
+    newItem.appendChild(newEditBox)//.setAttribute('id','newEditBox' )
+                        // ^ set the attribute so you can call it to a function ^ 
+    
+    newItem.appendChild(newDeleteButton)//.setAttribute('id', 'newDeleteButton')
+
+
+                            // simpler method here....
+    newDeleteButton.addEventListener('click', ()=>{newItem.remove()})
+                                            // add () to keep the event from firing.
+                                    //without the () my 'click' listener doesnt matter
+    
+    
+                // new item appended to the list with edit and delete buttons
+
+                // makinging the delete button function
+    
+                // let deleteButton = document.querySelector('')
+    
+                // deleteButton.addEventListener('click', function(e){
+        
+                // .remove()
+  })
+//console.log(newItem) 
+//})
+
+//function deleteItem(ID){
+//    document.getElementById('list').remove(document.getElementById(ID))
+
+    
+//}
